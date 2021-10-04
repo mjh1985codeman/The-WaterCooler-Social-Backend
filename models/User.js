ee217ebdfc9d7157a4761c23a0cdf32f429955d7
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-04 18:25:00 -0500

    began work on user model, server JS and user-controller

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+let validateEmail = function (email) {
+  let regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return regex.test(email);
+};
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validateEmail, "Please use a valid email address"],
+    match: [
+      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      "Please use a valid email address",
+    ],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      //Could I do User.username here to pull the username?
+    },
+  ],
+});
+
+// get total count of the user's friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
